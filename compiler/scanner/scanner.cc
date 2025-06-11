02e85f6057e96648252b00343503ebcd1c723ea9
Christian Brendlin <ch@brendlinonline.de>
2025-06-11 10:11:34 +0200

    Fix formatting: add newline at end of file in Scanner class

--- compiler/scanner/scanner.cc
+++ compiler/scanner/scanner.cc
@@ -1,3 +1,17 @@
 #include "scanner_internal.hh"
 
 bool Scanner::isAtEnd() { return current_offset >= source_buffer.length(); }
+
+char Scanner::advance() {
+  if (isAtEnd())
+    return '\0';
+
+  char temp_char;
+
+  temp_char = source_buffer[current_offset];
+
+  current_offset++;
+  current_column++;
+
+  return temp_char;
+}
